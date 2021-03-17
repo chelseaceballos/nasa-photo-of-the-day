41e95aec97cb687149a3731e5c1385a828364378
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-03-17 15:02:54 -0700

    mvp day 1

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function home () {
+    return (
+        <h1>Astronomy Picture of the Day!</h1>
+    );
+}
+export default home 
+
+// STEP 8: import react from 'react'
+
+// STEP: 9 create a function of home with a return of <h1>
+
