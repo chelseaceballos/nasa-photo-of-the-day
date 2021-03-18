b5bbc68fb5c1c980f0d271cf3ae6626d741f5ca7
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-03-18 11:19:16 -0700

    Day 2 of NPOTD

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
   <div className='App'>
     {Home()}
-    {NasaPhotos ({'data':data}) }
+    {NasaPhotos ({'data':data})}
   </div>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -9,5 +9,9 @@ export default home
 
 // STEP 8: import react from 'react'
 
-// STEP: 9 create a function of home with a return of <h1>
+// STEP: 9 create a function of home with a return of <h1>Title Here</h1>
+
+// STEP 10: export default home
+
+// set up NasaPhotos.js or context 
 
